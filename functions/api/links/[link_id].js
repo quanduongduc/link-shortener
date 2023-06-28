8768bd44480c7c9b5201a76f52232d836b5d64fd
quanduong <quanduong@maxflowtech.com>
2023-06-28 15:08:07 +0700

    add get link by shortened link

--- functions/api/links/[link_id].js
+++ functions/api/links/[link_id].js
@@ -3,15 +3,17 @@ import { baseResponse } from "../../utils"
 export async function onRequestGet(context) {
     try {
         const { request, env } = context
-        const link_id = context.params.link_id;
-        const { results } = await env.DB.prepare("SELECT * FROM links WHERE links.id = ? LIMIT 1").bind(link_id).all()
-        if (results[0]) {
+        const link_id = context.params.link_id
+        const { results } = await env.DB.prepare(`
+            SELECT * FROM links WHERE links.id = ? LIMIT 1;
+        `).bind(link_id).all()
+        if (results) {
             const headers = {
                 location: results[0].original_url
             }
             return await baseResponse(301, undefined, headers)
         }
-        return await baseResponse(500, "fail to create link, please try again")
+        return await baseResponse(500, "not found")
     } catch (error) {
         return await context.next()
     }
