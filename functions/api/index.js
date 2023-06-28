5e1ff650bdbbe3705fbef949c37910229296027c
quanduong <quanduong@maxflowtech.com>
2023-06-28 10:15:32 +0700

    fix wrong import format

--- functions/api/index.js
+++ functions/api/index.js
@@ -1,8 +1,8 @@
-import {baseResponse} from utils
+import { baseResponse } from "utils"
 
 export function onRequestGet(context) {
   try {
-    return baseResponse(200,"hello world");
+    return baseResponse(200, "hello world");
   } catch (error) {
     context.error = error;
     context.next(context);
